1e1a3c11415d70a303c3fcb9fd5f595bbd5aba70
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-06-23 19:59:02 +0900

    미션1 - 네이버로그인

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -13,9 +13,12 @@ const user = {
 
 */
 
+const inputEmail = document.querySelector('.user-email-input')
+const inputPassword = document.querySelector('.user-password-input')
+
+
 function emailReg(text){
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
   return re.test(String(text).toLowerCase())
 }
 
@@ -23,6 +26,29 @@ function pwReg(text){
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
+
+
+
+// email 정규표현식을 사용한 validation
+function emailVali(text) {
+  return emailReg(text) === false ? inputEmail.classList.add('is--invalid') : inputEmail.classList.remove('is--invalid')
+}
+
+// pw 정규표현식을 사용한 validation
+function pwVali(text) {
+  return pwReg(text) === false ? inputPassword.classList.add('is--invalid') : inputPassword.classList.remove('is--invalid')
+}
+
+
+// 로그인 버튼을 클릭시 조건처리
+function login(id, pw) {
+  if(id === user.id && pw === user.pw) window.location.href = 'welcome.html'
+}
+
+login(inputEmail.value, inputPassword.value)
+
+
+
 
 
 
